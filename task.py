492f70ab46af6e9ea4d52f94b16147bba59bc2ca
vinogradov-da <vdana406@gmail.com>
2024-12-13 17:52:50 +0300

    Update task.py

--- task.py
+++ task.py
@@ -8,21 +8,21 @@ class Smartphone:
         self.memory = memory
     def get_info(self) -> str:
         """
-        Получение информации о телефоне.
-
+        Возвращает информацию о телефоне.
         >>> phone = Smartphone("Apple", "iPhone 12", 256)
-        >>>phone.get_info()
+        >>> phone.get_info()
         'Smartphone от Apple, iPhone 12, объем памяти 256 Gb'
         """
-        return f'Smartphone от {self.brand}, {self.model} {self.memory} Gb
+        return f'Smartphone от {self.brand}, {self.model}, объем памяти {self.memory} Gb'
 
     def set_memory(self, new_memory: int) -> None:
         """
-         Устанавливает новый объем памяти.
-
-         >>> phone = Smartphone("Apple", "iPhone 12", 256)
-         >>> phone.set_memory(512)
-         """
+        Устанавливает новый объем памяти.
+        >>> phone = Smartphone("Apple", "iPhone 12", 256)
+        >>> phone.set_memory(512)
+        >>> phone.memory
+        512
+        """
         if new_memory < 0:
             raise ValueError("Объем памяти не может быть отрицательным")
         self.memory = new_memory
@@ -36,25 +36,25 @@ class Automobile:
 
     def get_info(self) -> str:
         """
-        Получение информации о машине
-
+        Возвращает информацию о машине.
         >>> car = Automobile("Audi A6", "белый", 60)
-        >>> car.get_info
-        'Audi A6, цвет белый, зафиксированная скорость - 60 км/ч'
+        >>> car.get_info()
+        'Audi A6, белый, зафиксированная скорость - 60 км/ч'
         """
         return f'{self.model}, {self.color}, зафиксированная скорость - {self.speed} км/ч'
 
     def change_speed (self, new_speed: int) -> None:
         """
-        Изменение скорости
-
+        Изменяем значение скорости.
         >>> car = Automobile("Audi A6", "белый", 60)
         >>> car.change_speed(50)
+        >>> car.speed
+        50
         """
         if not isinstance(new_speed, (int, float)):
-            raise TypeError("Добавляемая жидкость должна быть типа int или float")
+            raise TypeError("Изменяемая скорость должна быть типа int или float")
         if new_speed < 0:
-            raise ValueError("Добавляемая жидкость должна положительным числом")
+            raise ValueError("Изменяемая скорость должна положительным числом")
         self.speed = new_speed
 
 
@@ -63,10 +63,9 @@ class Table:
         self.height = height
         self.length = length
 
-    def get_info(self):
+    def get_info(self) -> str:
         """
-        Параметры стола
-
+        Возвращает информацию о столе.
         >>> folding_table = Table(70, 150)
         >>> folding_table.get_info()
         'Высота стола 70 см, длина - 150 см'
@@ -75,15 +74,16 @@ class Table:
 
     def increase_length(self, new_length) -> None:
         """
-        Изменяем длину стола
-
+        Изменяем длину стола.
         >>> folding_table = Table(70, 150)
         >>> folding_table.increase_length(200)
+        >>> folding_table.length
+        200
         """
         if not isinstance(new_length, (int, float)):
-            raise TypeError("Добавляемая жидкость должна быть типа int или float")
+            raise TypeError("Значение длинны должна быть типа int или float")
         if new_length < 0:
-            raise ValueError("Добавляемая жидкость должна положительным числом")
+            raise ValueError("Значение длинны должна быть положительным числом")
         self.length = new_length
 
 
